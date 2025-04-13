74fa6461078818ccd5a66741b2076b3f36dc0dab
Primrose <primrose.njoki@student.moringaschool.com>
2025-04-14 00:00:27 +0300

    first list

---
+++ src/Components/ExpenseList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ExpenseItem from './ExpenseItem';
+const ExpenseList=({expenses}) => { //takes expenses prop & maps through it to render each item
+    
+    // basic HTML table
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Expense</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((item)=>(
+                    <ExpenseItem key={item.id} item={item}/> ))}
+
+            </tbody>
+        </table>
+    );
+};
+    
+export default ExpenseList;
