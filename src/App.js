90b9dd89384f535d63826012db0b7a6e84629830
Primrose <primrose.njoki@student.moringaschool.com>
2025-04-13 20:46:30 +0300

    first app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'; //imported for state management
+import ExpenseForm from './ExpenseForm';// These are the2 imported components
+import ExpenseList from './ExpenseList';
 import './App.css';
 
+//Empty Arrays:
+//expense holds the expense items
+//setExpense updates the list
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [expenses,setExpenses] = useState([]);
+  
+  const addExpense =(expense) => {  //adds new expenses
+    setExpenses ([expenses, ...expenses]);
 
-export default App;
+    // header;passing addExpense with a prop(onAddExpense);passing expenses with a prop(expenses)
+    return (
+      <div className='container'>
+        <h1>Expense Tracker</h1> 
+        <ExpenseForm onAddExpense={addExpense}/>      passing addExpense with a prop(onAddExpense)
+        <ExpenseList expenses={expenses}/>
+      
+      </div>
+    );
+
+
+  }
+}
+export default App; 
